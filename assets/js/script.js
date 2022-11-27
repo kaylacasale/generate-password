3e96fbe31100d9cececbf369de2764b2e5b65f53
Kayla Casale <kayla.casale@gmail.com>
2022-11-26 23:58:34 -0800

    added part about upper case letters

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,25 @@ function getLength() {
 
     //message.innerHTML = "Your password will be " + inputLength.value + " characters long.";
 
-    if (inputLength >= 8 && inputLength <= 128) {
-        message.innerHTML = "Your password will be " + inputLength + " characters long.";
-    } else {
+    if (inputLength <= 8 && inputLength >= 128) {
+
         message.innerHTML = "Please enter a number between 8 and 128"
         return getLength();
+    } else {
+        message.innerHTML = "Your password will be " + inputLength + " characters long.";
     }
 }
 
+function getUpperCaseNumber() {
+    var inputUpperCaseNumber = document.querySelector("#inputUpperCaseNumber").value;
+    var messageUpperCase = document.querySelector("#messageUpperCase");
 
+    if (inputUpperCaseNumber = 0 || inputUpperCaseNumber >= userInputLength) {
+        messageUpperCase.innerHTML = "Please choose a number greater than 0 and less than chosen length.";
+    } else {
+        messageUpperCase.innerHTML = "Your password will have " + inputUpperCaseNumber.value + " UPPER case letters.";
+    }
+}
 
 
 
