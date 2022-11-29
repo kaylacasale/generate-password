deba1af6170c44deb7a7b64d03ecefc2a86be4a6
Kayla Casale <kayla.casale@gmail.com>
2022-11-29 01:29:37 -0800

    deleted draft files

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,45 @@ var errorEl = document.querySelector('.errorMessage');
 var numberInputEl = document.querySelector('.length');
 var changePEl = document.querySelector('.inputFor');
 var changePasswordEl = document.querySelector('.password');
-var numberLength;
+var numberLength = Number(numberInputEl.value);
 var passwordLength;
 //var lengthOutput = lengthFunction(numberLength);
 
+// check input value is equal to numberLength variable
+console.log(numberLength);
 
 enterLengthEl.addEventListener('click', function () {
-    numberLength = Number(numberInputEl.value);
+    numberLength;
     console.log(numberLength);
+    getLength();
+});
 
+// if numberLength is not a value, display error message
+function getLength() {
+    if (!numberLength) {
+        errorEl.innerHTML = "";
+        console.log("No input!");
+        errorEl.innerHTML = "ERROR: Not a number, please enter a value within the given range above.";
+        return numberLength;
+    } else if (
+        (numberLength < 8) || (numberLength > 128)) {
+        errorEl.innerHTML = ""
+        errorEl.innerHTML = 'ERROR: Your number needs to be within the range 8 - 128';
+    } else {
+        errorEl.innerHTML = "";
+        changePasswordEl.textContent = numberLength + " characters long";
+        changePEl.textContent = "Press to generate a random number of UPPER case letters for your secure password.";
+        return numberLength;
+    }
+}
 
+getLength(numberLength);
+/*
     if (!numberLength) {
         errorEl.innerHTML = "";
         console.log("You made it!");
         errorEl.innerHTML = "ERROR: Not a number, please enter a value within the given range above.";
-
+ 
     } else if (
         (numberLength < 8) || (numberLength > 128)) {
         errorEl.innerHTML = ""
@@ -28,65 +52,64 @@ enterLengthEl.addEventListener('click', function () {
         changePasswordEl.textContent = numberLength + " characters long";
         changePEl.textContent = "Press to generate a random number of UPPER case letters for your secure password.";
         return numberLength;
-
-
+ 
+ 
         //document.querySelector('.password').innerHTML = numberLength + ' characters long';
     }
 });
-
-lengthFunction();
-console.log(numberLength);
-
+ 
+ 
+ 
 //getLength(numberLength);
-
+ 
 //console.log(document.querySelector(numberLength));
 //var charactersLong =
 //errorEl. //to access global variable after input in function
-
+ 
 //console.log(numberLengthValue.numberLength.value);
 //this.numberLength.value = var fullLength;
 //numberLengthValue(numberLength);
-
+ 
 //numberLengthValue.numberLength();
 //lengthEmpty.unshift(numberLengthValue.numberLength.value);
 //console.log(lengthFull);
-
+ 
 //numberLengthValue(numberLength) = var passwordLength;
-
-
+ 
+ 
 /*
 var inputLength;
-
+ 
 function getLength() {
     var inputLength = document.querySelector("#inputLength").value;
     var message = document.querySelector("#message");
-
-
-
+ 
+ 
+ 
     //message.innerHTML = "Your password will be " + inputLength.value + " characters long.";
-
+ 
     if (inputLength <= 8 && inputLength >= 128) {
-
+ 
         message.innerHTML = "Please enter a number between 8 and 128"
         return getLength();
     } else {
         message.innerHTML = "Your password will be " + inputLength + " characters long.";
     }
 }
-
+ 
 function getUpperCaseNumber() {
     var inputUpperCaseNumber = document.querySelector("#inputUpperCaseNumber").value;
     var messageUpperCase = document.querySelector("#messageUpperCase");
-
+ 
     if (inputUpperCaseNumber = 0 || inputUpperCaseNumber >= userInputLength) {
         messageUpperCase.innerHTML = "Please choose a number greater than 0 and less than chosen length.";
     } else {
         messageUpperCase.innerHTML = "Your password will have " + inputUpperCaseNumber.value + " UPPER case letters.";
     }
 }
-
-
-
+ 
+ 
+ 
 //store <input> in variable inputLength
 //#finds the text from id element
 //empty until I press button, <h1> tag id messageLength responds when press button
