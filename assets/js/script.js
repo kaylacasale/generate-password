d60f563f19d5f84e944a275d3c455b57d4913403
Kayla Casale <kayla.casale@gmail.com>
2022-12-01 21:44:32 -0800

    added JS code to generate random selection of characters from each array type  given desired length input

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,16 +49,17 @@ randomUpperBtn.addEventListener('click', function (e) {
         pElError.textContent = "ERROR: Cannot begin generating without length number input";
     } else {
         for (var i = 0; i < length; i++) {
-            random = upperCaseLetters[Math.floor(Math.random() * upperCaseLettersLength)];
+            var letter = upperCaseLetters[Math.floor(Math.random() * upperCaseLettersLength)];
             console.log(random);
             var liTagUpper = document.createElement("p")
-            liTagUpper.textContent = random;
+            liTagUpper.textContent = letter;
             randomUpperText.appendChild(liTagUpper)
             addUpper.textContent = "ADD Upper Cases";
             addUpper.setAttribute("style", "background-color:greenyellow");
-            randomUppers.push(random);
-            console.log(randomUppers);
+            randomUppers.push(letter);
+            random.push(letter);
         }
+        console.log(random);
     }
 
 });
@@ -80,16 +81,17 @@ randomLowerBtn.addEventListener('click', function (e) {
         pElError.textContent = "ERROR: Cannot begin generating without length number input";
     } else {
         for (var i = 0; i < length; i++) {
-            random = lowerCaseLetters[Math.floor(Math.random() * lowerCaseLettersLength)];
+            var letter = lowerCaseLetters[Math.floor(Math.random() * lowerCaseLettersLength)];
             console.log(random);
             var liTagLower = document.createElement("p")
-            liTagLower.textContent = random;
+            liTagLower.textContent = letter;
             randomLowerText.appendChild(liTagLower)
             addLower.textContent = "ADD Lower Cases";
             addLower.setAttribute("style", "background-color:greenyellow");
-            randomLowers.push(random);
-            console.log(randomLowers);
+            randomLowers.push(letter);
+            random.push(letter);
         }
+        console.log(random)
     }
 });
 
@@ -112,16 +114,17 @@ randomSpecialBtn.addEventListener('click', function (e) {
         pElError.textContent = "ERROR: Cannot begin generating without length number input";
     } else {
         for (var i = 0; i < length; i++) {
-            random = specialChars[Math.floor(Math.random() * specialCharsLength)];
+            var letter = specialChars[Math.floor(Math.random() * specialCharsLength)];
             console.log(random);
             var liTagSpecial = document.createElement("p")
-            liTagSpecial.textContent = random;
+            liTagSpecial.textContent = letter;
             randomSpecialText.appendChild(liTagSpecial)
             addSpecial.textContent = "ADD Specials";
             addSpecial.setAttribute("style", "background-color:greenyellow");
-            randomSpecials.push(random);
-            console.log(randomSpecials);
+            randomSpecials.push(letter);
+            random.push(letter);
         }
+        console.log(random)
     }
 });
 
@@ -143,16 +146,17 @@ randomNumbersBtn.addEventListener('click', function (e) {
         pElError.textContent = "ERROR: Cannot begin generating without length number input";
     } else {
         for (var i = 0; i < length; i++) {
-            random = Numbers[Math.floor(Math.random() * NumbersLength)];
+            var letter = Numbers[Math.floor(Math.random() * NumbersLength)];
             console.log(random);
             var liTagNumbers = document.createElement("p")
-            liTagNumbers.textContent = random;
+            liTagNumbers.textContent = letter;
             randomNumbersText.appendChild(liTagNumbers)
             addNumber.textContent = "ADD Numbers";
             addNumber.setAttribute("style", "background-color:greenyellow");
-            randomNumbers.push(random);
-            console.log(randomNumbers);
+            randomNumbers.push(letter);
+            random.push(letter);
         }
+        console.log(random);
     }
 });
 
@@ -165,7 +169,7 @@ var divPassword = document.getElementById('addPassword');
 addBtnUpper.addEventListener('click', function (e) {
     e.preventDefault();
     console.log("Adding Random Values");
-    var DivTagPassword = document.createElement("label")
+    var DivTagPassword = document.createElement("p")
     DivTagPassword.textContent = "\n" + randomUppers;
     divPassword.appendChild(DivTagPassword);
 });
@@ -178,7 +182,7 @@ var addBtnLower = document.querySelector(".addLower")
 addBtnLower.addEventListener('click', function (e) {
     e.preventDefault();
     console.log("Adding Random Values");
-    var DivTagPassword = document.createElement("label")
+    var DivTagPassword = document.createElement("p")
     DivTagPassword.textContent = "\n" + randomLowers;
     divPassword.appendChild(DivTagPassword);
 });
@@ -190,7 +194,7 @@ var addBtnSpecial = document.querySelector(".addSpecial")
 addBtnSpecial.addEventListener('click', function (e) {
     e.preventDefault();
     console.log("Adding Random Values");
-    var DivTagPassword = document.createElement("label")
+    var DivTagPassword = document.createElement("p")
     DivTagPassword.textContent = "\n" + randomSpecials;
     divPassword.appendChild(DivTagPassword);
 });
@@ -202,7 +206,7 @@ var addBtnNumber = document.querySelector(".addNumber")
 addBtnNumber.addEventListener('click', function (e) {
     e.preventDefault();
     console.log("Adding Random Values");
-    var DivTagPassword = document.createElement("label")
+    var DivTagPassword = document.createElement("p")
     DivTagPassword.textContent = "\n" + randomNumbers;
     divPassword.appendChild(DivTagPassword);
 });
@@ -218,7 +222,7 @@ var randomAll = []
 
 addedCharactersBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    randomizeAll();
+    chooseRandom();
 
     // randomAll.push(randomUppers);
     // randomAll.push(randomLowers);
@@ -229,21 +233,42 @@ addedCharactersBtn.addEventListener('click', function (e) {
     // console.log(randomAll);
 
 });
+var pElDisplay = document.querySelector(".passwordDisplay");
 
-
-var randomAllTogether = randomAll.join();
 length = Number(lengthInput.value);
-var all = []
-// var upperRanomized = randomUppers.join();
-var choose = length.length / 4
-var chosenU = randomUppers[0, choose];
+function chooseRandom() {
+    for (var i = 0; i < length; i++) {
+        var choosePassword = random[Math.floor(Math.random() * random.length)];
+        randomAll.push(choosePassword);
+    }
+    console.log(randomAll);
+    pElDisplay.textContent = randomAll;
+
+}
 
 
-function randomizeAll() {
-    console.log(chosenU);
+// var divTagDisplay = document.querySelector(".passwordDisplay");
+
+// function displayPassword() {
+//     var displayTag = document.createElement("p");
+//     displayTag.textContent = randomAll;
+//     divTagDisplay.appendChlid(displayTag);
+//}
 
 
 
 
+// var randomAllTogether = randomAll.join();
+// length = Number(lengthInput.value);
+// var all = []
+// // var upperRanomized = randomUppers.join();
+// var choose = length.length / 4
+// var chosenU = randomUppers[0, choose];
 
-};
+
+// function randomizeAll() {
+//     console.log(chosenU);
+
+
+
+
